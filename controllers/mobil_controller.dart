cf2cf1b3c807266e3dd3432e1f4e5f43976d68e8
kelviin382 <145515088+kelviin020803@users.noreply.github.com>
2025-10-27 07:29:32 +0700

    Add files via upload

---
+++ controllers/mobil_controller.dart
@@ -0,0 +1,41 @@
+import 'package:get/get.dart'; //get x biar data bisa update otomatis di ui
+import '../models/mobil_model.dart'; //untuk bentuk data mobil
+import '../services/api_service.dart'; //untuk manggil api nya
+
+class MobilController extends GetxController {
+  var mobilList = <Mobil>[].obs; //tempat menyimpan data mobil dari api
+  var loading = false.obs; //penanda saat mengambil data, true saat loading
+  var error = ''.obs; //nyimpan pesan kalau ada error,
+
+  // fetch using http + async-await, jadi ini fungsi untuk mengambil data api makai library http
+  Future<void> fetchWithHttp() async {
+    loading.value = true;
+    error.value = '';
+    mobilList.clear();
+    try {
+      final data = await ApiService.getMobilHttp();
+      final list = data.map((e) => Mobil.fromJson(e)).toList();
+      mobilList.assignAll(list);
+    } catch (e) {
+      error.value = e.toString();
+    } finally {
+      loading.value = false;
+    }
+  }
+
+  // fetch using dio + callback chaining
+  Future<void> fetchWithDio() async {
+    loading.value = true;
+    error.value = '';
+    mobilList.clear();
+    try {
+      final data = await ApiService.getMobilDio();
+      final list = data.map((e) => Mobil.fromJson(e)).toList();
+      mobilList.assignAll(list);
+    } catch (e) {
+      error.value = e.toString();
+    } finally {
+      loading.value = false;
+    }
+  }
+}
